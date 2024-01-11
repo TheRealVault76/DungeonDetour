cf40b448b2988ba207cc15d27f55f408ca6e1fa7
TheRealVault76 <austingooch2003@gmail.com>
2024-01-11 12:14:52 -0600

    Inital Commit

---
+++ src/main/java/net/vault/dungeondetour/DungeonDetourDataGenerator.java
@@ -0,0 +1,11 @@
+package net.vault.dungeondetour;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DungeonDetourDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/vault/dungeondetour/DungeonDetourModClient.java
@@ -0,0 +1,11 @@
+package net.vault.dungeondetour;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DungeonDetourModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+}
